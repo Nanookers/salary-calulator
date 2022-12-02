40c62e39e167bd872430bbbfd317f63e129aa321
Connor Clark <connordeclark@gmail.com>
2022-12-02 17:43:53 -0600

    buttons are established

--- Holy Trinity/client.js
+++ Holy Trinity/client.js
@@ -1,5 +1,33 @@
-$(document).ready(onReady)
+$(document).ready(onReady);
 
 function onReady() {
     console.log(`hi`);
+    $(`#enterButton`).on(`click`, enterInputInformation);
+}
+
+let listOfEmployees = [];
+
+function enterInputInformation() { //successfully takes unput data and pushes it into an array
+    console.log(`hi`);
+    let firstName = $(`#firstNameInput`).val()
+    let lastName = $(`#lastNameInput`).val()
+    let idNumber = $(`#idNumberInput`).val()
+    let jobTitle = $(`#jobTitleInput`).val()
+    let salaryNumber = $(`#salaryInput`).val()
+    
+    let employeeObject = {
+        first: firstName,
+        last: lastName,
+        id: idNumber,
+        title: jobTitle,
+        salary: salaryNumber
+    }
+    listOfEmployees.push(employeeObject);
+
+}
+
+function renderEmployeeList() {
+    $('#employeeList').empty();
+
+    for()
 }
